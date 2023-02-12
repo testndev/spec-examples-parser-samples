06f8a349980da2d9301c64d994083ce0579bf307
Alhusaine NEMER <al@testndev.com>
2023-02-12 10:19:07 +0100

    add HTML reporter for Jest

--- with-jest/jest.config.js
+++ with-jest/jest.config.js
@@ -1,5 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  reporters: [
+    "summary",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+      },
+    ],
+  ],
 };
